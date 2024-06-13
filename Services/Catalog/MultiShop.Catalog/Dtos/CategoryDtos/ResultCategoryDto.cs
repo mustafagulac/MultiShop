7d07af32231ed67cbdc9d8506891341ff95560ef
Mustafa Gülaç <gulacmustafa1@gmail.com>
2024-06-13 23:20:54 +0300

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/CategoryDtos/ResultCategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiShop.Catalog.Dtos.CategoryDtos
+{
+    //kategori islerinde listelemek istedigimiz prop'lari tutan yer
+    public class ResultCategoryDto
+    {
+        public string CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
