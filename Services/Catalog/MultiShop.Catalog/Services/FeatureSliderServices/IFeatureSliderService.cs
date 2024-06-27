b313dd1e894fb8e75332b9b13e05795e6416a8c8
Mustafa Gülaç <gulacmustafa1@gmail.com>
2024-06-27 03:35:13 +0300

    the slider on the home page has evolved into a dynamic structure

---
+++ Services/Catalog/MultiShop.Catalog/Services/FeatureSliderServices/IFeatureSliderService.cs
@@ -0,0 +1,15 @@
+﻿using MultiShop.Catalog.Dtos.FeatureSliderDtos;
+
+namespace MultiShop.Catalog.Services.FeatureSliderServices
+{
+    public interface IFeatureSliderService
+    {
+        Task<List<ResultFeatureSliderDto>> GetAllFeatureSliderAsync();
+        Task<GetByIdFeatureSliderDto> GetByIdFeatureSliderAsync(string id);
+        Task CreateFeatureSliderAsync(CreateFeatureSliderDto FeatureSliderDto);
+        Task UpdateFeatureSliderAsync(UpdateFeatureSliderDto FeatureSliderDto);
+        Task DeleteFeatureSliderAsync(string id);
+        Task FeatureSliderChangeStatusToTrue(string id);
+        Task FeatureSliderChangeStatusToFalse(string id);
+    }
+}
