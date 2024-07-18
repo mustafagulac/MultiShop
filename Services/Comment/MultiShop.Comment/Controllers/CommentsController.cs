6c72f64b6b6034a441d4b15fc33cd95ca9301c4f
Mustafa Gülaç <gulacmustafa1@gmail.com>
2024-07-18 17:51:44 +0300

    in the section where product reviews are located, the sections where the comments appear have been made dynamic.

---
+++ Services/Comment/MultiShop.Comment/Controllers/CommentsController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Comment.Context;
+using MultiShop.Comment.Entities;
+
+namespace MultiShop.Comment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class CommentsController : ControllerBase
+    {
+        private readonly CommentContext _context;
+
+        public CommentsController(CommentContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult CommentList()
+        {
+            var values = _context.UserComments.ToList();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCommentById(int id) 
+        {
+            var value = _context.UserComments.Find(id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult CreateComment(UserComment userComment)
+        {
+            _context.UserComments.Add(userComment);
+            _context.SaveChanges();
+            return Ok("Yorum basariyla eklendi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateComment(UserComment userComment)
+        {
+            _context.UserComments.Update(userComment);
+            _context.SaveChanges();
+            return Ok("Yorum basariyla guncellendi");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteComment(int id) 
+        {
+            var value = _context.UserComments.Find(id);
+            _context.UserComments.Remove(value);
+            _context.SaveChanges();
+            return Ok("Yorum basariyla silindi");
+        }
+
+        [HttpGet("CommentListByProductId")]
+        public IActionResult CommentListByProductId(string id) 
+        {
+            var value = _context.UserComments.Where(x => x.ProductId == id).ToList();
+            return Ok(value);
+        }
+
+
+    }
+}
