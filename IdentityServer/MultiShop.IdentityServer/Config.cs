9a579bd494d39f9630de61bcf82979df01af9a2d
Mustafa Gülaç <gulacmustafa1@gmail.com>
2024-08-06 18:19:51 +0300

    UI order operations

--- IdentityServer/MultiShop.IdentityServer/Config.cs
+++ IdentityServer/MultiShop.IdentityServer/Config.cs
@@ -66,7 +66,7 @@ namespace MultiShop.IdentityServer
                 ClientName = "Multi Shop Manager User",
                 AllowedGrantTypes= GrantTypes.ResourceOwnerPassword,
                 ClientSecrets = {new Secret("multishopsecret".Sha256())},
-                AllowedScopes= { "CatalogReadPermission", "CatalogFullPermission", "BasketFullPermission" , "OcelotFullPermission" , "CommentFullPermission" , "PaymentFullPermission" , "ImagesFullPermission","DiscountFullPermission",
+                AllowedScopes= { "CatalogReadPermission", "CatalogFullPermission", "BasketFullPermission" , "OcelotFullPermission" , "CommentFullPermission" , "PaymentFullPermission" , "ImagesFullPermission","DiscountFullPermission","OrderFullPermission",
                 IdentityServerConstants.LocalApi.ScopeName,
                 IdentityServerConstants.StandardScopes.Email,
                 IdentityServerConstants.StandardScopes.OpenId,
