2619d235aa25f220d6c73abd37bb8e9a810f7183
Mustafa Gülaç <gulacmustafa1@gmail.com>
2024-08-08 12:04:02 +0300

    creating the design of the credit card payment page (template script)

---
+++ Frontends/MultiShop.WebUI/Controllers/PaymentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            @ViewBag.directory1 = "MultiShop";
+            @ViewBag.directory2 = "Ödeme İşlemleri";
+            @ViewBag.directory3 = "Banka/Kredi Kartı İle Ödeme";
+            return View();
+        }
+    }
+}
